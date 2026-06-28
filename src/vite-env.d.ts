b0c4b5dd90be39a6f5b20ba7b25e272fd008b41c
Kalachand Dash <vickyrocks392@gmail.com>
2026-06-28 16:08:39 +0530

    Refactor Firebase configuration to use environment variables

---
+++ src/vite-env.d.ts
@@ -0,0 +1,17 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  VITE_FIREBASE_API_KEY: string;
+  VITE_FIREBASE_AUTH_DOMAIN: string;
+  VITE_FIREBASE_PROJECT_ID: string;
+  VITE_FIREBASE_STORAGE_BUCKET: string;
+  VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+  VITE_FIREBASE_APP_ID: string;
+  VITE_FIREBASE_MEASUREMENT_ID: string;
+  VITE_FIREBASE_FIRESTORE_DATABASE_ID: string;
+  // add other env variables as needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
