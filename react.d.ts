1c4d863b09099f8899262b19958e60eeae17b7a7
Kalachand Dash <vickyrocks392@gmail.com>
2026-06-28 17:29:12 +0530

    Fix TypeScript configuration and remove temporary React typings

--- react.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "react" {
-  export const useState: any;
-  export const useEffect: any;
-  export type FC<P = {}> = any;
-  export type RefObject<T> = any;
-  export type ReactNode = any;
-}
-declare module "react/jsx-runtime";
